0225bf815e0fe46be58d40148898e7b0291a245f
Sergey <lazybark.dev@gmail.com>
2023-06-22 16:32:51 +0300

    Serializer fo FSE

--- pkg/fse/fsevent.go
+++ pkg/fse/fsevent.go
@@ -3,12 +3,26 @@
 
 package fse
 
-//FSEvent represents and event that can occur in the filesystem. It contains an object data
-//and event type
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// FSEvent represents and event that can occur in the filesystem. It contains an object data
+// and event type
 type FSEvent struct {
 	//Object is the object that was the target of the action
 	Object FSObject
 
 	//Action is the Action that was made over the object
 	Action FSAction
+}
+
+func (e FSEvent) JSON() ([]byte, error) {
+	ebg, err := json.Marshal(e)
+	if err != nil {
+		return ebg, fmt.Errorf("[FSEvent->JSON] %w", err)
+	}
+
+	return ebg, nil
 }
--- pkg/fse/fsobject.go
+++ pkg/fse/fsobject.go
@@ -1,6 +1,11 @@
 package fse
 
-//FSObject represents and filesystem object that can be watched by IFilesystemWatcher
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// FSObject represents and filesystem object that can be watched by IFilesystemWatcher
 type FSObject struct {
 	//Path holds full filesystem path from root to the file itself
 	Path string
@@ -11,4 +16,13 @@ type FSObject struct {
 	//Hash describes the object internals. Objects with same hash + type should be considered
 	//identical.
 	Hash string
+}
+
+func (o FSObject) JSON() ([]byte, error) {
+	ebg, err := json.Marshal(o)
+	if err != nil {
+		return ebg, fmt.Errorf("[FSObject->JSON] %w", err)
+	}
+
+	return ebg, nil
 }
