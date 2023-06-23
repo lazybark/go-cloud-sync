4db3b1634bc3e446d88ce7e205170b652de6fb77
Sergey <lazybark.dev@gmail.com>
2023-06-23 14:03:48 +0300

    Create models.go

---
+++ pkg/storage/sqlitestorage/models.go
@@ -0,0 +1,20 @@
+package sqlitestorage
+
+import "time"
+
+type (
+	FSObject struct {
+		ID          uint   `gorm:"primaryKey"`
+		Name        string `gorm:"uniqueIndex:file"`
+		Path        string `gorm:"uniqueIndex:file"`
+		ParentID    uint
+		Hash        string
+		IsDir       bool
+		Owner       int
+		Ext         string
+		Size        int64
+		FSUpdatedAt time.Time
+		CreatedAt   time.Time
+		UpdatedAt   time.Time
+	}
+)
