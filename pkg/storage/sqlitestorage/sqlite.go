82573f61d47a48a944a75bebae87237138271f78
Sergey <lazybark.dev@gmail.com>
2023-06-23 14:03:41 +0300

    Update sqlite.go

--- pkg/storage/sqlitestorage/sqlite.go
+++ pkg/storage/sqlitestorage/sqlite.go
@@ -1,30 +1,87 @@
 package sqlitestorage
 
-import "github.com/lazybark/go-cloud-sync/pkg/fse"
+import (
+	"log"
+	"os"
 
-func NewSQLite(path string) *SQLiteStorage {
+	"github.com/lazybark/go-cloud-sync/pkg/fse"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	gLogger "gorm.io/gorm/logger"
+)
+
+func NewSQLite(path string) (*SQLiteStorage, error) {
 	s := &SQLiteStorage{}
-	return s
+
+	db, err := gorm.Open(sqlite.Open("fs_watcher.db"), &gorm.Config{Logger: gLogger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		gLogger.Config{LogLevel: gLogger.Silent},
+	)})
+	if err != nil {
+		return nil, err
+	}
+	s.db = db
+
+	err = s.db.AutoMigrate(FSObject{})
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
 
 type SQLiteStorage struct {
+	db *gorm.DB
 }
 
-func (s *SQLiteStorage) CreateObject(obj fse.FSObject) (string, error) {
-	var hash string
-	return hash, nil
-}
-
-func (s *SQLiteStorage) RemoveObject(obj fse.FSObject) error {
+func (s *SQLiteStorage) CreateObject(obj fse.FSObject) error {
+	o := FSObject{
+		Path:        obj.Path,
+		Name:        obj.Name,
+		IsDir:       obj.IsDir,
+		Hash:        obj.Hash,
+		Ext:         obj.Ext,
+		Size:        obj.Size,
+		FSUpdatedAt: obj.UpdatedAt,
+	}
+	if err := s.db.Create(&o).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
-func (s *SQLiteStorage) UpdateObject(obj fse.FSObject) (string, error) {
-	var newHash string
-	return newHash, nil
+func (s *SQLiteStorage) RemoveObject(obj fse.FSObject) error {
+	if err := s.db.Where("name = ? and path = ?", obj.Name, obj.Path).Delete(&FSObject{}).Error; err != nil && err != gorm.ErrRecordNotFound {
+		return err
+	}
+
+	return nil
 }
 
-func (s *SQLiteStorage) AddOrUpdateObject(obj fse.FSObject) (string, error) {
-	var hash string
-	return hash, nil
+func (s *SQLiteStorage) UpdateObject(obj fse.FSObject) error {
+	return s.AddOrUpdateObject(obj)
+}
+
+func (s *SQLiteStorage) AddOrUpdateObject(obj fse.FSObject) error {
+	o := FSObject{}
+	if err := s.db.Where("name = ? and path = ?", obj.Name, obj.Path).First(&o).Error; err != nil && err != gorm.ErrRecordNotFound {
+		return err
+	}
+	if o.ID != 0 {
+		//Only specific fields can be updated
+		o.Hash = obj.Hash
+		o.Size = obj.Size
+		o.UpdatedAt = obj.UpdatedAt
+
+		if err := s.db.Save(&o).Error; err != nil {
+			return err
+		}
+	} else {
+		if err := s.CreateObject(obj); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
