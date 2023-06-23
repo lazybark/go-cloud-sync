f8ecad3f206d6aa073c123ee0986f48db977c31d
Sergey <lazybark.dev@gmail.com>
2023-06-23 14:03:54 +0300

    Update i_storage.go

--- pkg/storage/i_storage.go
+++ pkg/storage/i_storage.go
@@ -5,15 +5,15 @@ import "github.com/lazybark/go-cloud-sync/pkg/fse"
 
 type IStorage interface {
 	//CreateObject creates a record about a filesystem object in database
-	CreateObject(obj fse.FSObject) (hash string, err error)
+	CreateObject(obj fse.FSObject) error
 
 	//RemoveObject removes the record about a filesystem object in database
 	RemoveObject(obj fse.FSObject) (err error)
 
 	//UpdateObject updates the record about a filesystem object in database
-	UpdateObject(obj fse.FSObject) (newHash string, err error)
+	UpdateObject(obj fse.FSObject) error
 
 	//AddOrUpdateObject creates a record about a filesystem object in database or updates if
 	//record exists. Returns success = true if any of actions were successful
-	AddOrUpdateObject(obj fse.FSObject) (hash string, err error)
+	AddOrUpdateObject(obj fse.FSObject) error
 }
